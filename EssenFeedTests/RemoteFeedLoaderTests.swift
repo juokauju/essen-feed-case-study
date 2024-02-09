a9872addb1c41c9d3efc02472991a90d3fe99c14
Justina Siaulyte <justina.siaulyte@protonmail.com>
2024-02-09 16:51:08 +0200

    replace stuccing by capturing values instead

--- EssenFeedTests/RemoteFeedLoaderTests.swift
+++ EssenFeedTests/RemoteFeedLoaderTests.swift
@@ -30,10 +30,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
+        client.error =
         
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](client)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -50,11 +53,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
         var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
