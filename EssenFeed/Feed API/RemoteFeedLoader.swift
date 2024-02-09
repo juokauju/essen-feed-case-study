acbc709ca46528c562c0ebb2add17e5e6b48cc27
Justina Siaulyte <justina.siaulyte@protonmail.com>
2024-02-09 17:33:52 +0200

    replace to httpclient result type

--- EssenFeed/Feed API/RemoteFeedLoader.swift
+++ EssenFeed/Feed API/RemoteFeedLoader.swift
@@ -6,7 +6,7 @@ public enum HTTPClientResult {
 }
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public class RemoteFeedLoader {
@@ -24,10 +24,11 @@ public class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+                case .success:
                 completion(.invalidData)
-            } else {
+            case.failure:
                 completion(.connectivity)
             }
         }
