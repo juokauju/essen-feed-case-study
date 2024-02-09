df9ffa075ffda6a374386d8790f828c8563d31f8
Justina Siaulyte <justina.siaulyte@protonmail.com>
2024-02-09 16:38:36 +0200

    Initial Commit

---
+++ EssenFeed/EssenFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssenFeedApp.swift
+//  EssenFeed
+//
+//  Created by Justina Šiaulytė on 2024-02-09.
+//
+
+import SwiftUI
+
+@main
+struct EssenFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
