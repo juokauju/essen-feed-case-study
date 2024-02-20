6e43b876ea932a4d7979e348a67c301f5106d210
Justina Siaulyte <justina.siaulyte@protonmail.com>
2024-02-20 15:43:51 +0200

    move the URLSessionHTTPClient (the system under test, or SUT) creation to a factory method to protect our tests from breaking changes

--- EssenFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssenFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -38,7 +38,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
-let url = URL(string: "http://any-url.com")!
+        let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -47,7 +47,7 @@ let url = URL(string: "http://any-url.com")!
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -56,12 +56,10 @@ let url = URL(string: "http://any-url.com")!
         let url = URL(string: "http://any-url.com")!
         let requestError = NSError(domain: "any error", code: 1)
         URLProtocolStub.stubs(data: nil, response: nil, error: requestError)
-        
-        let sut = URLSessionHTTPClient()
-        
+
         let exp = expectation(description: "Wait for completion")
         
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case .failure(let receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, requestError.domain)
@@ -72,10 +70,14 @@ let url = URL(string: "http://any-url.com")!
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 1.0)
+        wait(for: [exp], timeout: 1.0)  
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
     
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
